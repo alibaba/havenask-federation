e6039d10990cc0017133ca91818478eca1cd975e
weizijun <weizijun.wzj@alibaba-inc.com>
2023-04-20 11:53:11 +0800

    kafka_start_timestamp必须配置为string类型

--- modules/havenask-engine/src/test/java/org/havenask/engine/index/config/RealtimeInfoTests.java
+++ modules/havenask-engine/src/test/java/org/havenask/engine/index/config/RealtimeInfoTests.java
@@ -25,7 +25,7 @@ public class RealtimeInfoTests extends HavenaskTestCase {
             "{\n"
                 + "\t\"bootstrap.servers\":\"localhost:9092\",\n"
                 + "\t\"data_table\":\"in0\",\n"
-                + "\t\"kafka_start_timestamp\":1000,\n"
+                + "\t\"kafka_start_timestamp\":\"1000\",\n"
                 + "\t\"module_name\":\"kafka\",\n"
                 + "\t\"module_path\":\"libbs_raw_doc_reader_plugin.so\",\n"
                 + "\t\"realtime_mode\":\"realtime_service_rawdoc_rt_build_mode\",\n"
