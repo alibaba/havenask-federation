1055b6222883d5fcb00aefe3585f805c69a70c3e
weizijun <weizijun.wzj@alibaba-inc.com>
2023-06-20 12:06:19 +0800

    配置更新时，升级目录version版本，解决qrs无法更新问题

--- elastic-fed/modules/havenask-engine/src/main/java/org/havenask/engine/index/config/BizConfig.java
+++ elastic-fed/modules/havenask-engine/src/main/java/org/havenask/engine/index/config/BizConfig.java
@@ -26,7 +26,7 @@ public class BizConfig {
     public boolean direct_write = true;
     public WalConfig wal_config = new WalConfig();
     public OnlineIndexConfig online_index_config = new OnlineIndexConfig();
-    public boolean realtime = true;
+    public boolean realtime = false;
 
     public static class OnlineIndexConfig {
         public boolean on_disk_flush_realtime_index = true;
