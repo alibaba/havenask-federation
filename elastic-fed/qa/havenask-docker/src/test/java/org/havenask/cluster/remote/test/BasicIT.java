13426902cc0e99c17bd2dfc4a0da9db44178c204
weizijun <weizijun.wzj@alibaba-inc.com>
2023-07-04 15:56:14 +0800

    添加启动havenask docker的集成测试用例框架

---
+++ elastic-fed/qa/havenask-docker/src/test/java/org/havenask/cluster/remote/test/BasicIT.java
@@ -0,0 +1,73 @@
+/*
+ *Copyright (c) 2021, Alibaba Group;
+ *Licensed under the Apache License, Version 2.0 (the "License");
+ *you may not use this file except in compliance with the License.
+ *You may obtain a copy of the License at
+
+ *   http://www.apache.org/licenses/LICENSE-2.0
+
+ *Unless required by applicable law or agreed to in writing, software
+ *distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *See the License for the specific language governing permissions and
+ *limitations under the License.
+ */
+
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*
+ * Modifications Copyright Havenask Contributors. See
+ * GitHub history for details.
+ */
+
+package org.havenask.cluster.remote.test;
+
+import org.havenask.action.admin.indices.delete.DeleteIndexRequest;
+import org.havenask.client.RequestOptions;
+import org.havenask.client.indices.CreateIndexRequest;
+import org.havenask.client.indices.GetIndexRequest;
+import org.havenask.client.indices.GetIndexResponse;
+import org.havenask.cluster.metadata.MappingMetadata;
+import org.havenask.common.collect.Map;
+import org.havenask.common.settings.Settings;
+import org.havenask.engine.index.engine.EngineSettings;
+
+import java.io.IOException;
+
+public class BasicIT extends AbstractHavenaskClusterTestCase {
+
+    public void testCRUD() throws IOException {
+        String index = "test1";
+        assertTrue(clusterClient().indices().create(
+            new CreateIndexRequest(index).settings(
+                Settings.builder().put(EngineSettings.ENGINE_TYPE_SETTING.getKey(), EngineSettings.ENGINE_HAVENASK)
+                    .build()),
+            RequestOptions.DEFAULT).isAcknowledged());
+
+        GetIndexResponse getIndexResponse = clusterClient().indices().get(new GetIndexRequest(index),
+            RequestOptions.DEFAULT);
+        assertEquals(getIndexResponse.getIndices().length, 1);
+        assertEquals(getIndexResponse.getSetting(index, EngineSettings.ENGINE_TYPE_SETTING.getKey()), EngineSettings.ENGINE_HAVENASK);
+        assertEquals(getIndexResponse.getSetting(index, "index.number_of_replicas"), "0");
+        assertEquals(getIndexResponse.getMappings().get(index), new MappingMetadata("_doc", Map.of("dynamic", "false")));
+
+        assertTrue(
+            clusterClient().indices().delete(new DeleteIndexRequest(index), RequestOptions.DEFAULT).isAcknowledged());
+    }
+}
