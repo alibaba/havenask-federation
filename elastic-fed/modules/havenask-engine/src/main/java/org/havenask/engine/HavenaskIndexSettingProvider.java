a8933111fa1c3c70c4c4f947c9a5a7081ec98439
weizijun <weizijun.wzj@alibaba-inc.com>
2023-07-07 14:55:19 +0800

    支持在refresh的时候检查并更新checkpoint (#134)

--- elastic-fed/modules/havenask-engine/src/main/java/org/havenask/engine/HavenaskIndexSettingProvider.java
+++ elastic-fed/modules/havenask-engine/src/main/java/org/havenask/engine/HavenaskIndexSettingProvider.java
@@ -15,12 +15,17 @@
 package org.havenask.engine;
 
 import org.havenask.common.settings.Settings;
+import org.havenask.common.unit.TimeValue;
 import org.havenask.engine.index.engine.EngineSettings;
 import org.havenask.index.shard.IndexSettingProvider;
 
 import static org.havenask.cluster.metadata.IndexMetadata.SETTING_NUMBER_OF_REPLICAS;
+import static org.havenask.index.IndexSettings.INDEX_REFRESH_INTERVAL_SETTING;
 
 public class HavenaskIndexSettingProvider implements IndexSettingProvider {
+    private static final String DEFAULT_REFRESH_INTERVAL = "30s";
+    private static final TimeValue MAX_REFRESH_INTERVAL = TimeValue.timeValueMinutes(5);
+
     public Settings getAdditionalIndexSettings(String indexName, boolean isDataStreamIndex, Settings templateAndRequestSettings) {
         if (EngineSettings.isHavenaskEngine(templateAndRequestSettings)) {
             Settings.Builder builder = Settings.builder();
@@ -29,6 +34,15 @@ public class HavenaskIndexSettingProvider implements IndexSettingProvider {
                 throw new IllegalArgumentException("havenask engine only support 0 replica");
             }
             builder.put(SETTING_NUMBER_OF_REPLICAS, 0);
+
+            if (false == templateAndRequestSettings.hasValue(INDEX_REFRESH_INTERVAL_SETTING.getKey())) {
+                builder.put(INDEX_REFRESH_INTERVAL_SETTING.getKey(), DEFAULT_REFRESH_INTERVAL);
+            } else {
+                TimeValue refresh = templateAndRequestSettings.getAsTime(INDEX_REFRESH_INTERVAL_SETTING.getKey(), null);
+                if (refresh.millis() > MAX_REFRESH_INTERVAL.millis()) {
+                    throw new IllegalArgumentException("havenask engine only support refresh interval less than " + MAX_REFRESH_INTERVAL);
+                }
+            }
 
             // 如果开启了realtime模式,则默认消费kafka的最新数据
             boolean realTime = EngineSettings.HAVENASK_REALTIME_ENABLE.get(templateAndRequestSettings);
