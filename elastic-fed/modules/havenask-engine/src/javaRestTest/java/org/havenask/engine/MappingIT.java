f2e190837208ea357181e2565b2ca23e320f358d
Huaixinww <141887897+Huaixinww@users.noreply.github.com>
2023-08-17 18:59:44 +0800

    增加mapping中havenask支持字段的测试样例 (#191)
    增加mapping中havenask支持的数据类型的测试样例
    测试了目前支持的十种数据类型，目前支持的数据类型为：
    boolean、byte、date、double、float、integer、keyword、long、short、text

--- elastic-fed/modules/havenask-engine/src/javaRestTest/java/org/havenask/engine/MappingIT.java
+++ elastic-fed/modules/havenask-engine/src/javaRestTest/java/org/havenask/engine/MappingIT.java
@@ -14,16 +14,83 @@
 
 package org.havenask.engine;
 
+import org.havenask.action.admin.cluster.health.ClusterHealthRequest;
+import org.havenask.action.admin.cluster.health.ClusterHealthResponse;
+import org.havenask.action.admin.indices.delete.DeleteIndexRequest;
 import org.havenask.client.RequestOptions;
 import org.havenask.client.indices.CreateIndexRequest;
+import org.havenask.client.indices.GetIndexRequest;
+import org.havenask.cluster.health.ClusterHealthStatus;
 import org.havenask.common.collect.Map;
 import org.havenask.common.settings.Settings;
 import org.havenask.engine.index.engine.EngineSettings;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 public class MappingIT extends AbstractHavenaskRestTestCase {
+    // test supported data type
+    public void testSupportedDataType() throws Exception {
+        String index = "index_supported_data_type";
+        // create index
+        assertTrue(
+            highLevelClient().indices()
+                .create(
+                    new CreateIndexRequest(index).settings(
+                        Settings.builder().put(EngineSettings.ENGINE_TYPE_SETTING.getKey(), EngineSettings.ENGINE_HAVENASK).build()
+                    )
+                        .mapping(
+                            Map.of(
+                                "properties",
+                                Map.of(
+                                    "dataTypeBoolean",
+                                    Map.of("type", "boolean"),
+                                    "dataTypeKeyword",
+                                    Map.of("type", "keyword"),
+                                    "dataTypeLong",
+                                    Map.of("type", "long"),
+                                    "dataTypeInteger",
+                                    Map.of("type", "integer"),
+                                    "dataTypeShort",
+                                    Map.of("type", "short"),
+                                    "dataTypeByte",
+                                    Map.of("type", "byte"),
+                                    "dataTypeDouble",
+                                    Map.of("type", "double"),
+                                    "dataTypeFloat",
+                                    Map.of("type", "float"),
+                                    "dataTypeDate",
+                                    Map.of("type", "date"),
+                                    "dataTypeText",
+                                    Map.of("type", "text")
+                                )/*,
+                                 "properties2",  //暂不支持的dataType
+                                 Map.of(
+                                        "dataTypeGeoPoint",
+                                        Map.of("type", "geo_point"),
+                                        "dataTypeGeoShape",
+                                        Map.of("type", "geo_shape"),
+                                        "dataTypeUnsignedLong",
+                                        Map.of("type", "unsigned_long")
+                                 )*/
+                            )
+                        ),
+                    RequestOptions.DEFAULT
+                )
+                .isAcknowledged()
+        );
+        assertBusy(() -> {
+            ClusterHealthResponse clusterHealthResponse = highLevelClient().cluster()
+                .health(new ClusterHealthRequest(index), RequestOptions.DEFAULT);
+            assertEquals(clusterHealthResponse.getStatus(), ClusterHealthStatus.GREEN);
+        }, 2, TimeUnit.MINUTES);
+
+        // delete index and HEAD index
+        assertTrue(highLevelClient().indices().delete(new DeleteIndexRequest(index), RequestOptions.DEFAULT).isAcknowledged());
+        assertEquals(false, highLevelClient().indices().exists(new GetIndexRequest(index), RequestOptions.DEFAULT));
+    }
+
     // test unsupported data type
     public void testUnsupportedDataType() throws Exception {
         String index = "index_unsupported_data_type";
