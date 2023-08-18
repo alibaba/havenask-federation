11269aab45a1eea7c5f78de32bad8a97fe8d9e6c
lvjiang <lvjiang.lj@alibaba-inc.com>
2023-08-18 19:19:56 +0800

    完善了flushIT

--- elastic-fed/modules/havenask-engine/src/javaRestTest/java/org/havenask/engine/FlushIT.java
+++ elastic-fed/modules/havenask-engine/src/javaRestTest/java/org/havenask/engine/FlushIT.java
@@ -30,6 +30,8 @@ package org.havenask.engine;
 
 import com.alibaba.fastjson.JSONObject;
 import org.apache.http.util.EntityUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.havenask.action.admin.cluster.health.ClusterHealthRequest;
 import org.havenask.action.admin.cluster.health.ClusterHealthResponse;
 import org.havenask.action.admin.indices.delete.DeleteIndexRequest;
@@ -44,27 +46,91 @@ import org.havenask.common.collect.Map;
 import org.havenask.common.settings.Settings;
 import org.havenask.common.xcontent.XContentType;
 import org.havenask.engine.index.engine.EngineSettings;
+import org.junit.AfterClass;
+import org.junit.Before;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class FlushIT extends AbstractHavenaskRestTestCase {
+
+    private static final long recordInterval = 1000;
+    private static final long waitDeleteTime = 0;
+    private static final long waitFlushTime = 10000;
+
+    // static logger
+    private static final Logger logger = LogManager.getLogger(FlushIT.class);
+
+    // claim a class contain index name and max_doc_count
+    private static class IndexAndMaxDocCount {
+        private String name;
+        private long maxDocCount;
+
+        IndexAndMaxDocCount(String name, long maxDocCount) {
+            this.name = name;
+            this.maxDocCount = maxDocCount;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public long getMaxDocCount() {
+            return maxDocCount;
+        }
+    }
+
+    // IndexAndMaxDocCount array
+    static IndexAndMaxDocCount[] indices = new IndexAndMaxDocCount[] {
+        new IndexAndMaxDocCount("flush_test", 5),
+        new IndexAndMaxDocCount("multi_flush_test", 2), };
+
+    @AfterClass
+    public static void cleanNeededIndex() {
+        try {
+            for (IndexAndMaxDocCount index : indices) {
+                if (highLevelClient().indices().exists(new GetIndexRequest(index.name), RequestOptions.DEFAULT)) {
+                    highLevelClient().indices().delete(new DeleteIndexRequest(index.name), RequestOptions.DEFAULT);
+                    logger.info("delete index {}", index);
+                }
+            }
+        } catch (IOException e) {
+            logger.error("delete index failed", e);
+        }
+    }
+
+    @Before
+    public void createNeededIndex() throws Exception {
+        for (IndexAndMaxDocCount index : indices) {
+            if (!highLevelClient().indices().exists(new GetIndexRequest(index.name), RequestOptions.DEFAULT)) {
+                try {
+                    assertTrue(
+                        highLevelClient().indices()
+                            .create(
+                                new CreateIndexRequest(index.name).settings(
+                                    Settings.builder()
+                                        .put(EngineSettings.ENGINE_TYPE_SETTING.getKey(), EngineSettings.ENGINE_HAVENASK)
+                                        .put("refresh_interval", "200ms")
+                                        .put("index.havenask.flush.max_doc_count", index.maxDocCount)
+                                        .build()
+                                ).mapping(Map.of("properties", Map.of("foo", Map.of("type", "keyword")))),
+                                RequestOptions.DEFAULT
+                            )
+                            .isAcknowledged()
+                    );
+                } catch (Exception e) {
+                    logger.error("create index {} failed", index, e);
+                }
+            }
+        }
+    }
+
     public void testFlush() throws Exception {
+        // Thread.sleep(waitDeleteTime);
         // create index with havenask engine and 1s refresh_interval
-        String index = "flush_test";
-        assertTrue(
-            highLevelClient().indices()
-                .create(
-                    new CreateIndexRequest(index).settings(
-                        Settings.builder()
-                            .put(EngineSettings.ENGINE_TYPE_SETTING.getKey(), EngineSettings.ENGINE_HAVENASK)
-                            .put("refresh_interval", "1s")
-                            .put("index.havenask.flush.max_doc_count", 5)
-                            .build()
-                    ).mapping(Map.of("properties", Map.of("foo", Map.of("type", "keyword")))),
-                    RequestOptions.DEFAULT
-                )
-                .isAcknowledged()
-        );
+
+        String index = indices[0].name;
+
         assertBusy(() -> {
             ClusterHealthResponse clusterHealthResponse = highLevelClient().cluster()
                 .health(new ClusterHealthRequest(index), RequestOptions.DEFAULT);
@@ -76,20 +142,23 @@ public class FlushIT extends AbstractHavenaskRestTestCase {
 
         // create 4 docs and all docs willed flushed
         for (int i = 0; i <= 3; i++) {
-            highLevelClient().index(new IndexRequest(index).source(Map.of("foo", "flush test " + i), XContentType.JSON), RequestOptions.DEFAULT);
+            highLevelClient().index(
+                new IndexRequest(index).source(Map.of("foo", "flush test " + i), XContentType.JSON),
+                RequestOptions.DEFAULT
+            );
         }
 
         assertEquals(-1, getLocalCheckpoint(index));
         assertEquals(3, getMaxSeqNo(index));
 
         // sleep 6s and create 2 docs, doc0 - doc4 will be flushed
-        Thread.sleep(6000);
+        Thread.sleep(recordInterval);
         for (int i = 4; i <= 5; i++) {
             highLevelClient().index(new IndexRequest(index).source(Map.of("foo", "flush test " + i)), RequestOptions.DEFAULT);
         }
 
         // wait for 10s for flush and create a doc to trigger flush
-        Thread.sleep(15000);
+        Thread.sleep(waitFlushTime);
         highLevelClient().index(new IndexRequest(index).source(Map.of("foo", "flush test 6")), RequestOptions.DEFAULT);
 
         // checkpoint will be 3 or 4, in most case it will be 3
@@ -102,30 +171,13 @@ public class FlushIT extends AbstractHavenaskRestTestCase {
         } else {
             fail("checkpoint should be 3 or 4, but get " + getLocalCheckpoint(index));
         }
-
-        // delete index and HEAD index
-        assertEquals(true, highLevelClient().indices().exists(new GetIndexRequest(index), RequestOptions.DEFAULT));
-        assertTrue(highLevelClient().indices().delete(new DeleteIndexRequest(index), RequestOptions.DEFAULT).isAcknowledged());
-        assertEquals(false, highLevelClient().indices().exists(new GetIndexRequest(index), RequestOptions.DEFAULT));
     }
 
     public void testMultiFlush() throws Exception {
+        // Thread.sleep(waitDeleteTime);
         // create index with havenask engine, 1s refresh_interval and 1 max_doc_count
-        String index = "multi_flush_test";
-        assertTrue(
-            highLevelClient().indices()
-                .create(
-                    new CreateIndexRequest(index).settings(
-                        Settings.builder()
-                            .put(EngineSettings.ENGINE_TYPE_SETTING.getKey(), EngineSettings.ENGINE_HAVENASK)
-                            .put("refresh_interval", "1s")
-                            .put("index.havenask.flush.max_doc_count", 2)
-                            .build()
-                    ).mapping(Map.of("properties", Map.of("foo", Map.of("type", "keyword")))),
-                    RequestOptions.DEFAULT
-                )
-                .isAcknowledged()
-        );
+        String index = indices[1].name;
+
         assertBusy(() -> {
             ClusterHealthResponse clusterHealthResponse = highLevelClient().cluster()
                 .health(new ClusterHealthRequest(index), RequestOptions.DEFAULT);
@@ -139,14 +191,14 @@ public class FlushIT extends AbstractHavenaskRestTestCase {
         // create 1 doc
         highLevelClient().index(new IndexRequest(index).source(Map.of("foo", "multi flush test 1")), RequestOptions.DEFAULT);
         assertEquals(0, getMaxSeqNo(index));
-        Thread.sleep(6000);
+        Thread.sleep(recordInterval);
 
         // create 2 doc, doc 0 and doc 1 will be flushed
         for (int i = 1; i <= 2; i++) {
             highLevelClient().index(new IndexRequest(index).source(Map.of("foo", "multi flush test " + i)), RequestOptions.DEFAULT);
         }
         // wait for 10s for flush and create a doc to trigger flush
-        Thread.sleep(15000);
+        Thread.sleep(waitFlushTime);
         highLevelClient().index(new IndexRequest(index).source(Map.of("foo", "multi flush test 3")), RequestOptions.DEFAULT);
 
         // checkpoint will be 0 or 1, in most case it will be 0
@@ -164,10 +216,10 @@ public class FlushIT extends AbstractHavenaskRestTestCase {
         assertEquals(3, getMaxSeqNo(index));
 
         // wait for 6s and create 1 doc, doc 2 and doc 3 will be flushed
-        Thread.sleep(6000);
+        Thread.sleep(recordInterval);
         highLevelClient().index(new IndexRequest(index).source(Map.of("foo", "multi flush test 4")), RequestOptions.DEFAULT);
         // wait for 10s for flush and create a doc to trigger flush
-        Thread.sleep(15000);
+        Thread.sleep(waitFlushTime);
         highLevelClient().index(new IndexRequest(index).source(Map.of("foo", "multi flush test 5")), RequestOptions.DEFAULT);
 
         // checkpoint will be 2 or 3, in most case it will be 2
@@ -187,12 +239,12 @@ public class FlushIT extends AbstractHavenaskRestTestCase {
         }
 
         // wait for 6s and create 2 doc, doc 16 will be flushed, doc 17 maybe flushed
-        Thread.sleep(6000);
+        Thread.sleep(recordInterval);
         for (int i = 17; i <= 18; i++) {
             highLevelClient().index(new IndexRequest(index).source(Map.of("foo", "multi flush test " + i)), RequestOptions.DEFAULT);
         }
         // wait for 15s for flush and create a doc to trigger flush
-        Thread.sleep(15000);
+        Thread.sleep(waitFlushTime);
         highLevelClient().index(new IndexRequest(index).source(Map.of("foo", "multi flush test 19")), RequestOptions.DEFAULT);
 
         // checkpoint will be 15, 16 or 17, in most case it will be 15 or 16
@@ -210,11 +262,6 @@ public class FlushIT extends AbstractHavenaskRestTestCase {
         }
         // check max_seq_no
         assertEquals(19, getMaxSeqNo(index));
-
-        // delete index and HEAD index
-        assertEquals(true, highLevelClient().indices().exists(new GetIndexRequest(index), RequestOptions.DEFAULT));
-        assertTrue(highLevelClient().indices().delete(new DeleteIndexRequest(index), RequestOptions.DEFAULT).isAcknowledged());
-        assertEquals(false, highLevelClient().indices().exists(new GetIndexRequest(index), RequestOptions.DEFAULT));
     }
 
     // get index stats
