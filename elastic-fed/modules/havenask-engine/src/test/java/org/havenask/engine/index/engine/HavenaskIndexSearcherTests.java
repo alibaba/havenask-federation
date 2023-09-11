9c076bae520c579d67ea83e43ca6247bd7863031
weizijun <weizijun.wzj@alibaba-inc.com>
2023-09-11 19:27:48 +0800

    query阶段，在ReaderContext中cache id映射 (#220)
    fetch拿到的是doc id列表，是int类型，通过在ReaderContext中cache id映射，根据doc
    id获取到主键id，再进行fetch查询
    
    update test case

--- elastic-fed/modules/havenask-engine/src/test/java/org/havenask/engine/index/engine/HavenaskIndexSearcherTests.java
+++ elastic-fed/modules/havenask-engine/src/test/java/org/havenask/engine/index/engine/HavenaskIndexSearcherTests.java
@@ -15,14 +15,21 @@
 package org.havenask.engine.index.engine;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.lucene.search.FieldDoc;
+import org.havenask.search.internal.ReaderContext;
 import org.havenask.search.query.QuerySearchResult;
 import org.havenask.test.HavenaskTestCase;
+import org.mockito.Mockito;
 
 public class HavenaskIndexSearcherTests extends HavenaskTestCase {
+    public static final String IDS_CONTEXT = "havenask_ids";
 
     public void testBuildQuerySearchResult() throws IOException {
+        ReaderContext readerContext1 = Mockito.mock(ReaderContext.class);
+        ReaderContext readerContext2 = Mockito.mock(ReaderContext.class);
+
         double delta = 0.0001;
         QuerySearchResult querySearchResult = new QuerySearchResult();
         String sqlResponseStr1 = "{\"total_time\":2.016,\"has_soft_failure\":false,\"covered_percent\":1.0,"
@@ -46,29 +53,47 @@ public class HavenaskIndexSearcherTests extends HavenaskTestCase {
             + "\"error_info\":{\"ErrorCode\":0,\"Error\":\"ERROR_NONE\",\"Message\":\"\"}}   ";
 
         String[] resStr1 = new String[] { "wRSgaYoBtIvm0jEE9eGc", "wBSgaYoBtIvm0jEE9OG2", "shRlY4oBtIvm0jEEEOFm" };
+        List<String> readerContextIds1 = new ArrayList<>(resStr1.length);
+        for (int i = 0; i < resStr1.length; i++) {
+            readerContextIds1.add(resStr1[i]);
+        }
+        Mockito.doNothing().when(readerContext1).putInContext(Mockito.any(String.class), Mockito.any());
+        Mockito.when(readerContext1.getFromContext(Mockito.eq(IDS_CONTEXT))).thenReturn(readerContextIds1);
         float[] resFloat1 = new float[] { 3.0F, 2.0F, 1.0F };
         int rowNum1 = 3;
-        HavenaskIndexSearcher.buildQuerySearchResult(querySearchResult, sqlResponseStr1);
+        HavenaskIndexSearcher.buildQuerySearchResult(querySearchResult, sqlResponseStr1, readerContext1);
         assertEquals(3L, querySearchResult.topDocs().topDocs.totalHits.value);
         assertEquals(3.0F, querySearchResult.getMaxScore(), delta);
+        List<String> ids1 = readerContext1.getFromContext(IDS_CONTEXT);
         for (int i = 0; i < rowNum1; i++) {
             assertEquals(resFloat1[i], querySearchResult.topDocs().topDocs.scoreDocs[i].score, delta);
-            assertEquals(0, querySearchResult.topDocs().topDocs.scoreDocs[i].shardIndex);
+            assertEquals(-1, querySearchResult.topDocs().topDocs.scoreDocs[i].shardIndex);
             assertEquals(i, querySearchResult.topDocs().topDocs.scoreDocs[i].doc);
-            assertEquals(resStr1[i], ((FieldDoc) querySearchResult.topDocs().topDocs.scoreDocs[i]).fields[0]);
+            assertEquals(resStr1[i], ids1.get(i));
         }
 
+        // ReaderContext readerContext2 = new ReaderContext(Mockito.mock(ShardSearchContextId.class), Mockito.mock(IndexService.class),
+        // Mockito.mock(IndexShard.class), Mockito.mock(Engine.SearcherSupplier.class), 0, true);
+        // ReaderContext readerContext2 = Mockito.mock(ReaderContext.class);
         String[] resStr2 = new String[] { "qwerty", "asdfgh", "zxcvbn", "yuiopl" };
+        List<String> readerContextIds2 = new ArrayList<>(resStr2.length);
+        for (int i = 0; i < resStr2.length; i++) {
+            readerContextIds2.add(resStr2[i]);
+        }
+        Mockito.doNothing().when(readerContext2).putInContext(Mockito.any(String.class), Mockito.any());
+        Mockito.when(readerContext2.getFromContext(Mockito.eq(IDS_CONTEXT))).thenReturn(readerContextIds2);
+
         float[] resFloat2 = new float[] { 4.0F, 3.0F, 2.0F, 1.0F };
         int rowNum2 = 4;
-        HavenaskIndexSearcher.buildQuerySearchResult(querySearchResult, sqlResponseStr2);
+        HavenaskIndexSearcher.buildQuerySearchResult(querySearchResult, sqlResponseStr2, readerContext2);
+        List<String> ids2 = readerContext2.getFromContext(IDS_CONTEXT);
         assertEquals(4L, querySearchResult.topDocs().topDocs.totalHits.value);
         assertEquals(4.0F, querySearchResult.getMaxScore(), delta);
         for (int i = 0; i < rowNum2; i++) {
             assertEquals(resFloat2[i], querySearchResult.topDocs().topDocs.scoreDocs[i].score, delta);
-            assertEquals(0, querySearchResult.topDocs().topDocs.scoreDocs[i].shardIndex);
+            assertEquals(-1, querySearchResult.topDocs().topDocs.scoreDocs[i].shardIndex);
             assertEquals(i, querySearchResult.topDocs().topDocs.scoreDocs[i].doc);
-            assertEquals(resStr2[i], ((FieldDoc) querySearchResult.topDocs().topDocs.scoreDocs[i]).fields[0]);
+            assertEquals(resStr2[i], ids2.get(i));
         }
     }
 }
