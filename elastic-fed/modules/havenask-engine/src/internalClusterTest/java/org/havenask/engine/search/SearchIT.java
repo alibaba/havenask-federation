ebdf452eab4d54b5f24d056a18ab483a4f053e8b
weizijun <weizijun.wzj@alibaba-inc.com>
2023-09-15 10:40:39 +0800

    spotless

--- elastic-fed/modules/havenask-engine/src/internalClusterTest/java/org/havenask/engine/search/SearchIT.java
+++ elastic-fed/modules/havenask-engine/src/internalClusterTest/java/org/havenask/engine/search/SearchIT.java
@@ -45,7 +45,7 @@ public class SearchIT extends HavenaskITTestCase {
     protected boolean addMockInternalEngine() {
         return false;
     }
-    
+
     public void testSearch() throws Exception {
         String index = "test2";
         String mapping = "{\n"
