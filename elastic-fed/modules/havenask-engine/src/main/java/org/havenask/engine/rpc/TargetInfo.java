b6d44bc0aa1a1ffde45f5c176cc3517817e9a2fa
weizijun <weizijun.wzj@alibaba-inc.com>
2023-09-27 16:33:19 +0800

    spotless

--- elastic-fed/modules/havenask-engine/src/main/java/org/havenask/engine/rpc/TargetInfo.java
+++ elastic-fed/modules/havenask-engine/src/main/java/org/havenask/engine/rpc/TargetInfo.java
@@ -37,9 +37,13 @@ public class TargetInfo {
     public static class AppInfo {
         @Override
         public boolean equals(Object o) {
-            if (this == o) {return true;}
-            if (o == null || getClass() != o.getClass()) {return false;}
-            AppInfo appInfo = (AppInfo)o;
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            AppInfo appInfo = (AppInfo) o;
             return keep_count == appInfo.keep_count && Objects.equals(config_path, appInfo.config_path);
         }
 
@@ -56,10 +60,15 @@ public class TargetInfo {
         public static class Biz {
             @Override
             public boolean equals(Object o) {
-                if (this == o) {return true;}
-                if (o == null || getClass() != o.getClass()) {return false;}
-                Biz biz = (Biz)o;
-                return keep_count == biz.keep_count && Objects.equals(config_path, biz.config_path)
+                if (this == o) {
+                    return true;
+                }
+                if (o == null || getClass() != o.getClass()) {
+                    return false;
+                }
+                Biz biz = (Biz) o;
+                return keep_count == biz.keep_count
+                    && Objects.equals(config_path, biz.config_path)
                     && Objects.equals(custom_biz_info, biz.custom_biz_info);
             }
 
@@ -91,9 +100,13 @@ public class TargetInfo {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) {return true;}
-            if (o == null || getClass() != o.getClass()) {return false;}
-            BizInfo bizInfo = (BizInfo)o;
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            BizInfo bizInfo = (BizInfo) o;
             return Objects.equals(default_biz, bizInfo.default_biz);
         }
 
@@ -113,9 +126,13 @@ public class TargetInfo {
 
             @Override
             public boolean equals(Object o) {
-                if (this == o) {return true;}
-                if (o == null || getClass() != o.getClass()) {return false;}
-                Service service = (Service)o;
+                if (this == o) {
+                    return true;
+                }
+                if (o == null || getClass() != o.getClass()) {
+                    return false;
+                }
+                Service service = (Service) o;
                 return Objects.equals(topo_info, service.topo_info);
             }
 
@@ -145,13 +162,20 @@ public class TargetInfo {
 
             @Override
             public boolean equals(Object o) {
-                if (this == o) {return true;}
-                if (o == null || getClass() != o.getClass()) {return false;}
-                cm2Config cm2Config = (cm2Config)o;
-                return part_count == cm2Config.part_count && version == cm2Config.version
+                if (this == o) {
+                    return true;
+                }
+                if (o == null || getClass() != o.getClass()) {
+                    return false;
+                }
+                cm2Config cm2Config = (cm2Config) o;
+                return part_count == cm2Config.part_count
+                    && version == cm2Config.version
                     && part_id == cm2Config.part_id
-                    && tcp_port == cm2Config.tcp_port && support_heartbeat == cm2Config.support_heartbeat
-                    && grpc_port == cm2Config.grpc_port && Objects.equals(biz_name, cm2Config.biz_name)
+                    && tcp_port == cm2Config.tcp_port
+                    && support_heartbeat == cm2Config.support_heartbeat
+                    && grpc_port == cm2Config.grpc_port
+                    && Objects.equals(biz_name, cm2Config.biz_name)
                     && Objects.equals(ip, cm2Config.ip);
             }
 
@@ -180,11 +204,18 @@ public class TargetInfo {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) {return true;}
-            if (o == null || getClass() != o.getClass()) {return false;}
-            ServiceInfo that = (ServiceInfo)o;
-            return part_id == that.part_id && part_count == that.part_count && version == that.version
-                && Objects.equals(cm2, that.cm2) && Objects.equals(zone_name, that.zone_name)
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            ServiceInfo that = (ServiceInfo) o;
+            return part_id == that.part_id
+                && part_count == that.part_count
+                && version == that.version
+                && Objects.equals(cm2, that.cm2)
+                && Objects.equals(zone_name, that.zone_name)
                 && Objects.equals(cm2_config, that.cm2_config);
         }
 
@@ -202,11 +233,14 @@ public class TargetInfo {
 
                 @Override
                 public boolean equals(Object o) {
-                    if (this == o) {return true;}
-                    if (o == null || getClass() != o.getClass()) {return false;}
-                    DeployStatus that = (DeployStatus)o;
-                    return deploy_status == that.deploy_status && Objects.equals(local_config_path,
-                        that.local_config_path);
+                    if (this == o) {
+                        return true;
+                    }
+                    if (o == null || getClass() != o.getClass()) {
+                        return false;
+                    }
+                    DeployStatus that = (DeployStatus) o;
+                    return deploy_status == that.deploy_status && Objects.equals(local_config_path, that.local_config_path);
                 }
 
                 @Override
@@ -231,25 +265,45 @@ public class TargetInfo {
 
             @Override
             public boolean equals(Object o) {
-                if (this == o) {return true;}
-                if (o == null || getClass() != o.getClass()) {return false;}
-                Partition partition = (Partition)o;
-                return deploy_status == partition.deploy_status && inc_version == partition.inc_version
-                    && keep_count == partition.keep_count && rt_status == partition.rt_status
-                    && schema_version == partition.schema_version && table_load_type == partition.table_load_type
-                    && table_status == partition.table_status && table_type == partition.table_type && Objects.equals(
-                    check_index_path, partition.check_index_path) && Objects.equals(deploy_status_map,
-                    partition.deploy_status_map) && Objects.equals(loaded_config_path, partition.loaded_config_path)
-                    && Objects.equals(loaded_index_root, partition.loaded_index_root) && Objects.equals(
-                    local_index_path, partition.local_index_path);
+                if (this == o) {
+                    return true;
+                }
+                if (o == null || getClass() != o.getClass()) {
+                    return false;
+                }
+                Partition partition = (Partition) o;
+                return deploy_status == partition.deploy_status
+                    && inc_version == partition.inc_version
+                    && keep_count == partition.keep_count
+                    && rt_status == partition.rt_status
+                    && schema_version == partition.schema_version
+                    && table_load_type == partition.table_load_type
+                    && table_status == partition.table_status
+                    && table_type == partition.table_type
+                    && Objects.equals(check_index_path, partition.check_index_path)
+                    && Objects.equals(deploy_status_map, partition.deploy_status_map)
+                    && Objects.equals(loaded_config_path, partition.loaded_config_path)
+                    && Objects.equals(loaded_index_root, partition.loaded_index_root)
+                    && Objects.equals(local_index_path, partition.local_index_path);
             }
 
             @Override
             public int hashCode() {
-                return Objects.hash(check_index_path, deploy_status, deploy_status_map, inc_version, keep_count,
+                return Objects.hash(
+                    check_index_path,
+                    deploy_status,
+                    deploy_status_map,
+                    inc_version,
+                    keep_count,
                     loaded_config_path,
-                    loaded_index_root, local_index_path, rt_status, schema_version, table_load_type, table_status,
-                    table_type);
+                    loaded_index_root,
+                    local_index_path,
+                    rt_status,
+                    schema_version,
+                    table_load_type,
+                    table_status,
+                    table_type
+                );
             }
         }
 
@@ -289,21 +343,41 @@ public class TargetInfo {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) {return true;}
-            if (o == null || getClass() != o.getClass()) {return false;}
-            TableInfo tableInfo = (TableInfo)o;
-            return table_mode == tableInfo.table_mode && table_type == tableInfo.table_type
-                && total_partition_count == tableInfo.total_partition_count && force_online == tableInfo.force_online
-                && rt_status == tableInfo.rt_status && timestamp_to_skip == tableInfo.timestamp_to_skip
-                && Objects.equals(config_path, tableInfo.config_path) && Objects.equals(group_name,
-                tableInfo.group_name) && Objects.equals(index_root, tableInfo.index_root) && Objects.equals(
-                partitions, tableInfo.partitions) && Objects.equals(raw_index_root, tableInfo.raw_index_root);
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            TableInfo tableInfo = (TableInfo) o;
+            return table_mode == tableInfo.table_mode
+                && table_type == tableInfo.table_type
+                && total_partition_count == tableInfo.total_partition_count
+                && force_online == tableInfo.force_online
+                && rt_status == tableInfo.rt_status
+                && timestamp_to_skip == tableInfo.timestamp_to_skip
+                && Objects.equals(config_path, tableInfo.config_path)
+                && Objects.equals(group_name, tableInfo.group_name)
+                && Objects.equals(index_root, tableInfo.index_root)
+                && Objects.equals(partitions, tableInfo.partitions)
+                && Objects.equals(raw_index_root, tableInfo.raw_index_root);
         }
 
         @Override
         public int hashCode() {
-            return Objects.hash(table_mode, table_type, total_partition_count, config_path, force_online, group_name,
-                index_root, partitions, raw_index_root, rt_status, timestamp_to_skip);
+            return Objects.hash(
+                table_mode,
+                table_type,
+                total_partition_count,
+                config_path,
+                force_online,
+                group_name,
+                index_root,
+                partitions,
+                raw_index_root,
+                rt_status,
+                timestamp_to_skip
+            );
         }
     }
 
@@ -312,9 +386,13 @@ public class TargetInfo {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) {return true;}
-            if (o == null || getClass() != o.getClass()) {return false;}
-            CatalogAddress that = (CatalogAddress)o;
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            CatalogAddress that = (CatalogAddress) o;
             return Objects.equals(catalog_address, that.catalog_address);
         }
 
