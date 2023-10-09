47eaf28585f04344df8a729ed1c06d1c9fcdda7b
Huaixinww <141887897+Huaixinww@users.noreply.github.com>
2023-10-09 10:05:32 +0800

    增加测试样例，反复创建、删除索引不再出现异常 (#264)

--- elastic-fed/modules/havenask-engine/src/javaRestTest/java/org/havenask/engine/BasicIT.java
+++ elastic-fed/modules/havenask-engine/src/javaRestTest/java/org/havenask/engine/BasicIT.java
@@ -14,11 +14,16 @@
 
 package org.havenask.engine;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.havenask.action.admin.cluster.health.ClusterHealthRequest;
 import org.havenask.action.admin.cluster.health.ClusterHealthResponse;
 import org.havenask.action.admin.indices.delete.DeleteIndexRequest;
+import org.havenask.action.get.GetRequest;
+import org.havenask.action.get.GetResponse;
+import org.havenask.action.index.IndexRequest;
 import org.havenask.client.RequestOptions;
 import org.havenask.client.ha.SqlClientInfoRequest;
 import org.havenask.client.ha.SqlClientInfoResponse;
@@ -29,6 +34,7 @@ import org.havenask.cluster.health.ClusterHealthStatus;
 import org.havenask.cluster.metadata.MappingMetadata;
 import org.havenask.common.collect.Map;
 import org.havenask.common.settings.Settings;
+import org.havenask.common.xcontent.XContentType;
 import org.havenask.engine.index.engine.EngineSettings;
 
 public class BasicIT extends AbstractHavenaskRestTestCase {
@@ -134,4 +140,141 @@ public class BasicIT extends AbstractHavenaskRestTestCase {
         assertEquals(false, highLevelClient().indices().exists(new GetIndexRequest(index), RequestOptions.DEFAULT));
     }
 
+    public void testCreateAndDeleteSameIndex() throws Exception {
+        int randomTimes = randomIntBetween(2, 6);
+        for (int i = 0; i < randomTimes; i++) {
+            int shardsNum = randomIntBetween(1, 6);
+            String index = "create_and_delete_test";
+            // create index
+            assertTrue(
+                highLevelClient().indices()
+                    .create(
+                        new CreateIndexRequest(index).settings(
+                            Settings.builder()
+                                .put(EngineSettings.ENGINE_TYPE_SETTING.getKey(), EngineSettings.ENGINE_HAVENASK)
+                                .put("index.number_of_shards", shardsNum)
+                                .put("number_of_replicas", 0)
+                                .build()
+                        ).mapping(Map.of("properties", Map.of("content" + i, Map.of("type", "keyword")))),
+                        RequestOptions.DEFAULT
+                    )
+                    .isAcknowledged()
+            );
+            assertBusy(() -> {
+                ClusterHealthResponse clusterHealthResponse = highLevelClient().cluster()
+                    .health(new ClusterHealthRequest(index), RequestOptions.DEFAULT);
+                assertEquals(clusterHealthResponse.getStatus(), ClusterHealthStatus.GREEN);
+            }, 2, TimeUnit.MINUTES);
+
+            // GET index
+            assertEquals(true, highLevelClient().indices().exists(new GetIndexRequest(index), RequestOptions.DEFAULT));
+            GetIndexResponse getIndexResponse = highLevelClient().indices().get(new GetIndexRequest(index), RequestOptions.DEFAULT);
+            assertEquals(
+                new MappingMetadata("_doc", Map.of("dynamic", "false", "properties", Map.of("content" + i, Map.of("type", "keyword")))),
+                getIndexResponse.getMappings().get(index)
+            );
+
+            // put doc
+            int randomDocNum = randomIntBetween(1, 4);
+            for (int j = 0; j < randomDocNum; j++) {
+                highLevelClient().index(
+                    new IndexRequest(index).id(String.valueOf(j)).source(Map.of("content" + i, "欢迎使用" + j), XContentType.JSON),
+                    RequestOptions.DEFAULT
+                );
+            }
+
+            // get doc
+            for (int j = 0; j < randomDocNum; j++) {
+                String curId = String.valueOf(j);
+                assertBusy(() -> {
+                    GetResponse getResponse = highLevelClient().get(new GetRequest(index, curId), RequestOptions.DEFAULT);
+                    assertEquals(true, getResponse.isExists());
+                }, 10, TimeUnit.SECONDS);
+
+                GetResponse getResponse = highLevelClient().get(new GetRequest(index, String.valueOf(j)), RequestOptions.DEFAULT);
+                assertEquals("欢迎使用" + j, getResponse.getSourceAsMap().get("content" + i));
+            }
+
+            // delete index
+            assertTrue(highLevelClient().indices().delete(new DeleteIndexRequest(index), RequestOptions.DEFAULT).isAcknowledged());
+            assertEquals(false, highLevelClient().indices().exists(new GetIndexRequest(index), RequestOptions.DEFAULT));
+        }
+    }
+
+    public void testCreateAndDeleteDiffIndex() throws Exception {
+        int randomNum = randomIntBetween(2, 6);
+        String baseName = "create_and_delete_test";
+        List<String> indexs = new ArrayList<>();
+        for (int i = 0; i < randomNum; i++) {
+            indexs.add(baseName + i);
+        }
+
+        // create indexs
+        for (int i = 0; i < randomNum; i++) {
+            int shardsNum = randomIntBetween(1, 6);
+            assertTrue(
+                highLevelClient().indices()
+                    .create(
+                        new CreateIndexRequest(indexs.get(i)).settings(
+                            Settings.builder()
+                                .put(EngineSettings.ENGINE_TYPE_SETTING.getKey(), EngineSettings.ENGINE_HAVENASK)
+                                .put("index.number_of_shards", shardsNum)
+                                .put("number_of_replicas", 0)
+                                .build()
+                        ).mapping(Map.of("properties", Map.of("content" + i, Map.of("type", "keyword")))),
+                        RequestOptions.DEFAULT
+                    )
+                    .isAcknowledged()
+            );
+        }
+
+        for (int i = 0; i < randomNum; i++) {
+            String curIndex = indexs.get(i);
+            assertBusy(() -> {
+                ClusterHealthResponse clusterHealthResponse = highLevelClient().cluster()
+                    .health(new ClusterHealthRequest(curIndex), RequestOptions.DEFAULT);
+                assertEquals(clusterHealthResponse.getStatus(), ClusterHealthStatus.GREEN);
+            }, 2, TimeUnit.MINUTES);
+        }
+
+        // get index
+        for (int i = 0; i < randomNum; i++) {
+            assertEquals(true, highLevelClient().indices().exists(new GetIndexRequest(indexs.get(i)), RequestOptions.DEFAULT));
+            GetIndexResponse getIndexResponse = highLevelClient().indices().get(new GetIndexRequest(indexs.get(i)), RequestOptions.DEFAULT);
+            assertEquals(
+                new MappingMetadata("_doc", Map.of("dynamic", "false", "properties", Map.of("content" + i, Map.of("type", "keyword")))),
+                getIndexResponse.getMappings().get(indexs.get(i))
+            );
+        }
+
+        // put and get doc
+        for (int i = 0; i < randomNum; i++) {
+            int randomDocNum = randomIntBetween(1, 4);
+            for (int j = 0; j < randomDocNum; j++) {
+                String curId = String.valueOf(i) + String.valueOf(j);
+                highLevelClient().index(
+                    new IndexRequest(indexs.get(i)).id(curId).source(Map.of("content" + i, "欢迎使用" + j), XContentType.JSON),
+                    RequestOptions.DEFAULT
+                );
+            }
+
+            for (int j = 0; j < randomDocNum; j++) {
+                String curId = String.valueOf(i) + String.valueOf(j);
+                String curIndex = indexs.get(i);
+                assertBusy(() -> {
+                    GetResponse getResponse = highLevelClient().get(new GetRequest(curIndex, curId), RequestOptions.DEFAULT);
+                    assertEquals(true, getResponse.isExists());
+                }, 10, TimeUnit.SECONDS);
+                GetResponse getResponse = highLevelClient().get(new GetRequest(curIndex, curId), RequestOptions.DEFAULT);
+                assertEquals(true, getResponse.isExists());
+                assertEquals("欢迎使用" + j, getResponse.getSourceAsMap().get("content" + i));
+            }
+        }
+
+        // delete index
+        for (int i = 0; i < randomNum; i++) {
+            assertTrue(highLevelClient().indices().delete(new DeleteIndexRequest(indexs.get(i)), RequestOptions.DEFAULT).isAcknowledged());
+            assertEquals(false, highLevelClient().indices().exists(new GetIndexRequest(indexs.get(i)), RequestOptions.DEFAULT));
+        }
+    }
 }
