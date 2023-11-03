db1e9f4a9659ae091a5c8f493a32c74fc694adea
weizijun <weizijun.wzj@alibaba-inc.com>
2023-11-03 15:38:47 +0800

    解决fastjson的load policy问题

--- elastic-fed/modules/havenask-engine/src/main/java/org/havenask/engine/util/JsonPrettyFormatter.java
+++ elastic-fed/modules/havenask-engine/src/main/java/org/havenask/engine/util/JsonPrettyFormatter.java
@@ -14,59 +14,29 @@
 
 package org.havenask.engine.util;
 
-import java.util.Map;
-
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.PropertyNamingStrategy;
-import com.alibaba.fastjson.TypeReference;
-import com.alibaba.fastjson.parser.ParserConfig;
-import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
 /**
- * TODO（zhixu.zt) 去掉fastjson做准备
- *
  * fastjson的格式化json格式，都放到这个了，
  * 后面去掉fastjson直接改这里就可以了；
  *
  * fastjson的parse接口，还需要提供一个接口出来
  */
 public class JsonPrettyFormatter {
-
-    private static final SerializeConfig gConfig;
-    private static final ParserConfig gDConfig;
-
-    static {
-        gConfig = new SerializeConfig();
-        gConfig.propertyNamingStrategy = PropertyNamingStrategy.SnakeCase;
-
-        gDConfig = new ParserConfig();
-        gDConfig.propertyNamingStrategy = PropertyNamingStrategy.SnakeCase;
-    }
-
-    public static <T> String toString(T object) {
-        return JSON.toJSONString(object, gConfig, SerializerFeature.PrettyFormat);
-    }
-
     public static <T> String toJsonString(T object) {
-        return JSON.toJSONString(object, SerializerFeature.PrettyFormat);
+        return AccessController.doPrivileged((PrivilegedAction<String>) () -> JSON.toJSONString(object, SerializerFeature.PrettyFormat));
     }
 
     public static <T> T fromJsonString(String jsonString, Class<T> clazz) {
         return JSON.parseObject(jsonString, clazz);
     }
 
-    public static <T> T fromString(String jsonString, Class<T> clazz) {
-        return JSON.parseObject(jsonString, clazz, gDConfig);
-    }
-
     public static JSONObject fromString(String jsonString) {
         return JSON.parseObject(jsonString);
-    }
-
-    public static Map<String, String> from(String jsonString) {
-        return JSONObject.parseObject(jsonString, new TypeReference<Map<String, String>>() {
-        });
     }
 }
