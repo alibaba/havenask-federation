f1b482dd7f13210e1d9f73922ee2478c13c26e61
weizijun <weizijun.wzj@alibaba-inc.com>
2023-11-28 10:51:44 +0800

    通过参数控制是否加载qrs target

--- elastic-fed/distribution/src/bin/havenask-command/general_search_starter.py
+++ elastic-fed/distribution/src/bin/havenask-command/general_search_starter.py
@@ -164,6 +164,7 @@ examples:
         self.parser.add_option('', '--searcherLocalSubscribe', action='store_true', dest='searcherLocalSubscribe', default=False)
         self.parser.add_option('', '--enablePublishTableTopoInfo', action='store_true', dest='enablePublishTableTopoInfo', default=False)
         self.parser.add_option('', '--force_tablet_load', action='store_true', dest='forceTabletLoad', default=False)
+        self.parser.add_option('', '--qrsLoadTarget', action='store_true', dest='qrsLoadTarget', default=False)
 
     def parseParams(self, optionList):
         self.optionList = optionList
@@ -400,16 +401,16 @@ examples:
                 json.dump(self.gigInfos, f)
 
         if self.role == "all" or self.role == "qrs":
-            with open(os.path.join(self.workdir, "readyZones.json"), "r") as f:
-                self.gigInfos = json.load(f)
             qrs_ret = self.startQrs()
             if qrs_ret != 0:
                 return qrs_ret, ("", "start qrs failed", -1)
 
-            ret = self.loadQrsTarget(terminator.left_time())
-            if ret != 0:
-                return ret, ("", "load qrs target failed", -1)
-
+            if self.qrsLoadTarget:
+                with open(os.path.join(self.workdir, "readyZones.json"), "r") as f:
+                    self.gigInfos = json.load(f)
+                ret = self.loadQrsTarget(terminator.left_time())
+                if ret != 0:
+                    return ret, ("", "load qrs target failed", -1)
 
         return 0, ("", "", 0)
 
