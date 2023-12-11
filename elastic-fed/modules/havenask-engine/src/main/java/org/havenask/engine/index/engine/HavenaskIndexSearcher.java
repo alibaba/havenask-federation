22953e101bfbf31d3e88da9d95303cf855d3d038
weizijun <weizijun.wzj@alibaba-inc.com>
2023-12-11 11:16:07 +0800

    请求fed接口替代直接访问qrs

--- elastic-fed/modules/havenask-engine/src/main/java/org/havenask/engine/index/engine/HavenaskIndexSearcher.java
+++ elastic-fed/modules/havenask-engine/src/main/java/org/havenask/engine/index/engine/HavenaskIndexSearcher.java
@@ -14,6 +14,10 @@
 
 package org.havenask.engine.index.engine;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.lucene.index.IndexReader;
@@ -26,12 +30,13 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.TotalHits;
 import org.apache.lucene.search.TotalHits.Relation;
 import org.apache.lucene.search.similarities.Similarity;
+import org.havenask.client.Client;
 import org.havenask.client.ha.SqlResponse;
 import org.havenask.common.Strings;
 import org.havenask.common.lucene.search.TopDocsAndMaxScore;
-import org.havenask.engine.rpc.QrsClient;
-import org.havenask.engine.rpc.QrsSqlRequest;
-import org.havenask.engine.rpc.QrsSqlResponse;
+import org.havenask.engine.search.action.HavenaskSqlAction;
+import org.havenask.engine.search.action.HavenaskSqlRequest;
+import org.havenask.engine.search.action.HavenaskSqlResponse;
 import org.havenask.engine.util.Utils;
 import org.havenask.index.shard.ShardId;
 import org.havenask.search.DefaultSearchContext;
@@ -40,24 +45,18 @@ import org.havenask.search.internal.ContextIndexSearcher;
 import org.havenask.search.internal.ReaderContext;
 import org.havenask.search.query.QuerySearchResult;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.havenask.engine.search.rest.RestHavenaskSqlAction.SQL_DATABASE;
-
 public class HavenaskIndexSearcher extends ContextIndexSearcher {
     private static final Logger logger = LogManager.getLogger(HavenaskIndexSearcher.class);
     public static final String IDS_CONTEXT = "havenask_ids";
     private static final int ID_POS = 0;
     private static final int SCORE_POS = 1;
-    private final QrsClient qrsHttpClient;
+    private final Client client;
     private final ShardId shardId;
     private final String tableName;
     private final DefaultSearchContext searchContext;
 
     public HavenaskIndexSearcher(
-        QrsClient qrsHttpClient,
+        Client client,
         ShardId shardId,
         DefaultSearchContext searchContext,
         IndexReader reader,
@@ -67,7 +66,7 @@ public class HavenaskIndexSearcher extends ContextIndexSearcher {
         boolean wrapWithExitableDirectoryReader
     ) throws IOException {
         super(reader, similarity, queryCache, queryCachingPolicy, wrapWithExitableDirectoryReader);
-        this.qrsHttpClient = qrsHttpClient;
+        this.client = client;
         this.shardId = shardId;
         this.tableName = Utils.getHavenaskTableName(shardId);
         this.searchContext = searchContext;
@@ -76,9 +75,7 @@ public class HavenaskIndexSearcher extends ContextIndexSearcher {
     @Override
     public void search(Query query, Collector collector) throws IOException {
         String sql = QueryTransformer.toSql(tableName, searchContext.request().source(), searchContext.indexShard().mapperService());
-        String kvpair = "format:full_json;timeout:10000;databaseName:" + SQL_DATABASE;
-        QrsSqlRequest request = new QrsSqlRequest(sql, kvpair);
-        QrsSqlResponse response = qrsHttpClient.executeSql(request);
+        HavenaskSqlResponse response = client.execute(HavenaskSqlAction.INSTANCE, new HavenaskSqlRequest(sql, null)).actionGet();
         if (false == Strings.isNullOrEmpty(response.getResult())) {
             SqlResponse sqlResponse = SqlResponse.parse(response.getResult());
             if (logger.isDebugEnabled()) {
