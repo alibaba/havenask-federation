94619ea3726e7cf3db12c270578fc275b4c3d781
weizijun <weizijun.wzj@alibaba-inc.com>
2024-01-22 19:04:59 +0800

    优化recovery流程，去掉directory的listall，防止其他地方调用性能差 (#429)

--- elastic-fed/modules/havenask-engine/src/main/java/org/havenask/engine/index/store/HavenaskDirectory.java
+++ elastic-fed/modules/havenask-engine/src/main/java/org/havenask/engine/index/store/HavenaskDirectory.java
@@ -15,12 +15,8 @@
 package org.havenask.engine.index.store;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.stream.Stream;
 
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FilterDirectory;
@@ -31,46 +27,6 @@ public class HavenaskDirectory extends FilterDirectory {
     protected HavenaskDirectory(Directory in, Path shardPath) {
         super(in);
         this.shardPath = shardPath;
-    }
-
-    @Override
-    public String[] listAll() throws IOException {
-        List<String> files = listHavenaskFiles();
-        files.addAll(List.of(in.listAll()));
-        return files.toArray(new String[0]);
-    }
-
-    List<String> listHavenaskFiles() throws IOException {
-        List<Path> files = retryListAllHavenaskFiles(shardPath);
-        String shardPathStr = shardPath.toString();
-        List<String> fileNames = new ArrayList<>();
-        files.forEach(path -> {
-            String fileName = path.toString().substring(shardPathStr.length() + 1);
-            fileNames.add(fileName);
-        });
-        return fileNames;
-    }
-
-    static List<Path> retryListAllHavenaskFiles(Path dir) {
-        try {
-            return listAllHavenaskFiles(dir);
-        } catch (IOException e) {
-            return retryListAllHavenaskFiles(dir);
-        }
-    }
-
-    static List<Path> listAllHavenaskFiles(Path dir) throws IOException {
-        List<Path> files = new ArrayList<>();
-        try (Stream<Path> stream = Files.list(dir)) {
-            stream.forEach(path -> {
-                if (Files.isDirectory(path)) {
-                    files.addAll(retryListAllHavenaskFiles(path));
-                } else {
-                    files.add(path);
-                }
-            });
-        }
-        return files;
     }
 
     @Override
